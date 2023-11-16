c29fba649051491dd70319ca6a6f784bfce16880
Valentino Alonso <valenalonso7@gmail.com>
2023-11-15 21:10:36 -0300

    first commit

---
+++ proyecto1/src/components/CartWidget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'animate.css'
+
+const CartWidget = () => {
+    return (
+      <div className="cart-widget">
+        <i className="icono-carrito">Icono de carrito</i>
+        <span className="notificacion">5</span>
+      </div>
+    );
+  };
+  
+  export default CartWidget;
